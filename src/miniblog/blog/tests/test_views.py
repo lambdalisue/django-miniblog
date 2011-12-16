db1a1686204fd7f89fc054e2ef01c91143f81c4b
Λlisue <lambdalisue@hashnote.net>
2011-12-16 17:13:59 +0900

    Add requirements.txt and test and autocmd

--- src/miniblog/blog/tests/test_views.py
+++ src/miniblog/blog/tests/test_views.py
@@ -25,26 +25,66 @@ License:
 """
 __AUTHOR__ = "lambdalisue (lambdalisue@hashnote.net)"
 from django.test import TestCase
+from ..models import Entry
 
 class EntryViewTestCase(TestCase):
     fixtures = ['test.yaml']
 
-    def test_list(self):
+    def test_list_get(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_detail(self):
+    def test_detail_get(self):
         response = self.client.get('/foo/')
         self.assertEqual(response.status_code, 200)
+    def test_detail_get_invalid(self):
+        response = self.client.get('/unknown/')
+        self.assertEqual(response.status_code, 404)
 
-    def test_create(self):
+    def test_create_get(self):
         response = self.client.get('/create/')
         self.assertEqual(response.status_code, 200)
+    def test_create_post(self):
+        response = self.client.post('/create/', {
+                'title': 'foobar', 'body': 'foobar'
+            })
+        self.assertEqual(response.status_code, 302)
+        assert Entry.objects.filter(title='foobar').exists()
+    def test_create_post_invalid(self):
+        response = self.client.post('/create/', {
+                'title': '', 'body': ''
+            })
+        self.assertEqual(response.status_code, 200)
+        assert 'This field is required' in response.content
 
-    def test_update(self):
+
+    def test_update_get(self):
         response = self.client.get('/update/1/')
         self.assertEqual(response.status_code, 200)
+    def test_update_get_invalid(self):
+        response = self.client.get('/update/999/')
+        self.assertEqual(response.status_code, 404)
+    def test_update_post(self):
+        response = self.client.post('/update/1/', {
+                'title': 'foobar', 'body': 'foobar'
+            })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Entry.objects.get(pk=1).title, 'foobar')
+        self.assertEqual(Entry.objects.get(pk=1).body, 'foobar')
+    def test_update_post_invalid(self):
+        response = self.client.post('/update/1/', {
+                'title': '', 'body': ''
+            })
+        self.assertEqual(response.status_code, 200)
+        assert 'This field is required' in response.content
 
-    def test_delete(self):
+    def test_delete_get(self):
         response = self.client.get('/delete/1/')
         self.assertEqual(response.status_code, 200)
+    def test_delete_get_invalid(self):
+        response = self.client.get('/delete/999/')
+        self.assertEqual(response.status_code, 404)
+    def test_delete_post(self):
+        response = self.client.post('/delete/1/')
+        self.assertEqual(response.status_code, 302)
+        assert not Entry.objects.filter(pk=1).exists()
